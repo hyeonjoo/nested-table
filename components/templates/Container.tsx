5cc185e5a934a1eaa9f84d4ff80d27e36c7c65b0
Hyunjoo <hyunjoopark0208@gmail.com>
2021-12-15 13:50:58 +0100

    chore: Container interface name change

--- components/templates/Container.tsx
+++ components/templates/Container.tsx
@@ -1,12 +1,12 @@
 import React, { ReactNode } from "react";
 import Head from "next/head";
 
-type Props = {
+interface ContainerProps {
   children?: ReactNode;
   title?: string;
-};
+}
 
-const Container = ({ children, title = "Hello World" }: Props) => (
+const Container = ({ children, title = "Hello World" }: ContainerProps) => (
   <div>
     <Head>
       <title>{title}</title>
